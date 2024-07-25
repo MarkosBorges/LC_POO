f3dc53537bcc2aafd844e3e873da618c270143ab
MaRkos <markous_k@protonmail.com>
2024-07-25 20:56:58 -0300

    commit 'EXERCICIO: PESSOA2 - AJUSTANDO DATAS'

--- EXERCICIOS/LISTA05_PESSOA2/src/Main.java
+++ EXERCICIOS/LISTA05_PESSOA2/src/Main.java
@@ -1,10 +1,17 @@
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
 
         int idadeAlbert,idadeNewton;
+
+        Locale br = new Locale("pt", "BR");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy", br);
+        LocalDate hoje = LocalDate.now();
+        String dataFormatada = hoje.format(formatter);
 
         Pessoa2 albert = new Pessoa2(14,3,1879,"Albert Einstein");
         Pessoa2 isaac = new Pessoa2(4,1,1643,"Isaac Newton");
@@ -16,10 +23,13 @@ public class Main {
         System.out.println("Nome: "+albert.informarNome());
         System.out.println("Data de Nascimento: "+albert.getDia()+"/"+albert.getMes()+"/"+albert.getAno());
         System.out.println("Idade dele no dia de hoje (caso tivessem vivos): "+idadeAlbert+" anos");
+        System.out.println("Data de Hoje: "+dataFormatada);
         System.out.println();
         System.out.println("========= IDADE NEWTON ========");
         System.out.println("Nome: "+isaac.informarNome());
         System.out.println("Data de Nascimento: "+isaac.getDia()+"/"+isaac.getMes()+"/"+isaac.getAno());
         System.out.println("Idade dele no dia de hoje (caso tivessem vivos): "+idadeNewton+ " anos");
+        System.out.println("Data de Hoje: "+dataFormatada);
+
     }
 }
--- EXERCICIOS/LISTA05_PESSOA2/src/Pessoa2.java
+++ EXERCICIOS/LISTA05_PESSOA2/src/Pessoa2.java
@@ -1,5 +1,7 @@
 import java.time.LocalDate;
 import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class Pessoa2 {
     int idade;
@@ -10,7 +12,6 @@ public class Pessoa2 {
     LocalDate nascimento;
 
     LocalDate hoje = LocalDate.now();
-
 
     public Pessoa2() {
     }
@@ -23,45 +24,18 @@ public class Pessoa2 {
         this.nascimento = LocalDate.of(ano, mes, dia);
     }
 
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
     public int getDia() {
         return dia;
-    }
-
-    public void setDia(int dia) {
-        this.dia = dia;
     }
 
     public int getMes() {
         return mes;
     }
 
-    public void setMes(int mes) {
-        this.mes = mes;
-    }
-
     public int getAno() {
         return ano;
     }
 
-    public void setAno(int ano) {
-        this.ano = ano;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 
     // ---------------------------------------------
     // UTILIZADO METODO BETWEEN PARA CALCULAR O NASC
@@ -73,10 +47,6 @@ public class Pessoa2 {
         } else {
             return 0;
         }
-    }
-
-    public int informarIdade() {
-        return this.idade;
     }
 
     public String informarNome() {
