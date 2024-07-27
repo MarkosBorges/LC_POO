39784760e9813f63896ecd7345a8eda9a9f34fd6
aluno <markos.borges@hotmail.com>
2024-07-26 21:14:37 -0300

    Commit 'TERMINANDO LISTA 6...'

---
+++ EXERCICIOS/LISTA06_PRODUTO/src/Main.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Produto produto = new Produto("CANETA", 10, 0.5);
+        Produto p2 = new Produto("LAPIS", 20, 0.3);
+        Produto p3 = new Produto("BORRACHA", 50, 1.5);
+
+        Scanner scanner = new Scanner(System.in);
+
+        int opcao;
+        do {
+            System.out.println("\nMenu:");
+            System.out.println("0. Fim");
+            System.out.println("1. Retirada");
+            System.out.println("2. Reposição");
+            System.out.println("3. Alterar preço unitário");
+            System.out.println("4. Exibir valor do estoque");
+            System.out.println("5. Exibir dados do produto");
+            System.out.print("Escolha uma opção: ");
+            opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 0:
+                    System.out.println("Fim do programa.");
+                    break;
+                case 1:
+                    System.out.print("Digite a quantidade a ser retirada: ");
+                    int quantidadeRetirada = scanner.nextInt();
+                    produto.retira(quantidadeRetirada);
+                    break;
+                case 2:
+                    System.out.print("Digite a quantidade a ser reposta: ");
+                    int quantidadeReposicao = scanner.nextInt();
+                    produto.repoe(quantidadeReposicao);
+                    break;
+                case 3:
+                    System.out.print("Digite o novo preço unitário: ");
+                    //double novoPreco = scanner.nextDouble();
+                    double novoPreco = Double.parseDouble();
+                    //produto.setPrecoUnit(novoPreco);
+                    produto.alteraPrecoUnitario(novoPreco);
+                    break;
+                case 4:
+                    System.out.println("Valor do estoque: R$ " + produto.obtemValorEstoque());
+                    break;
+                case 5:
+                    System.out.println(produto);
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+            }
+        } while (opcao != 0);
+
+        scanner.close();
+        System.out.println("FIM!");
+
+    }
+}
---
+++ EXERCICIOS/LISTA06_PRODUTO/src/Produto.java
@@ -0,0 +1,90 @@
+public class Produto {
+    // Declaração do(s) atributo(s)
+    private String descricao; // descrição do produto
+    private int quant; // quantidade do produto em estoque
+    private double precoUnit; // preço unitário do produto
+
+    public Produto(){}
+
+    public Produto(String descricao, int quant, double precoUnit) {
+        this.descricao = descricao;
+        this.quant = quant;
+        this.precoUnit = precoUnit;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getQuant() {
+        return quant;
+    }
+
+    public void setQuant(int quant) {
+        this.quant = quant;
+    }
+
+    public double getPrecoUnit() {
+        return precoUnit;
+    }
+
+    public void setPrecoUnit(double precoUnit) {
+        this.precoUnit = precoUnit;
+    }
+
+    /* Método para atualizar o estoque com a quantidade passada como
+    parâmetro. Caso a quantidade passada como parâmetro for zero ou
+    negativa o estoque não deve ser atualizado */
+
+    public void repoe(int quantRep) {
+        if (quantRep > 0){
+            quant = quant + quantRep;
+        }
+    }
+
+
+    /* Método para atualizar o estoque com a quantidade passada como
+    parâmetro. Caso a quantidade passada como parâmetro for zero,
+    negativa ou superior ao estoque existente, a atualização não deve
+    ser executada */
+    public boolean retira(int quantRet) {
+        if ((quantRet > 0) && (quantRet <= getQuant())){
+            quant -= quantRet;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public void alteraPrecoUnitario(double novoPreco){
+        if(novoPreco > 0.0){
+            precoUnit = precoUnit + novoPreco;
+            //System.out.println("Novo preço atualizado!");
+            //return novoPreco;
+        }else {
+            //System.out.println("Não foi possivel atualizar!");
+        }
+    }
+
+
+    /* Método para retornar o valor total do estoque considerando a
+    quantidade existente e o preço unitário do produto. */
+    public double obtemValorEstoque() {
+        return (getQuant() * getPrecoUnit());
+    }
+
+    //-------------------
+    @Override
+    public String toString() {
+        return "\nProduto{" +
+                "Descricao: '" + getDescricao() + '\'' +
+                ", Quantidade em Estoque: " + getQuant() +
+                ", Valor: " + getPrecoUnit() +
+                '}';
+    }
+}
